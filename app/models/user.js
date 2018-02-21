0e145fb9d7885123e9fa6156e4cfaf5f0366e1ee
Ilmir Bakirov <ilmirbakirov@gmail.com>
2018-02-21 16:02:06 +0500

    added user schema, configured passwords through bcrypt, wrote post endpoint for saving new user

---
+++ app/models/user.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+var UserSchema = new Schema({
+  username: {
+    type: String,
+    lowercase: true,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    required: true,
+    unique: true
+  }
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if(err){
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -3,18 +3,50 @@ var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var User = require('./app/models/user');
+var bodyParser = require('body-parser');
 
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // console logging requests
 app.use(morgan('dev'));
 
-mongoose.connect('mongodb://localhost:27017/mean-tutorial', function (err) {
-  if(err){
-    console.log("Not connected to the db: ",err);
-  }else{
+mongoose.connect('mongodb://localhost:27017/mean-tutorial', function(err) {
+  if (err) {
+    console.log("Not connected to the db: ", err);
+  } else {
     console.log("Successfully connected to db");
   }
 });
 
-app.listen(port, function () {
+// http://localhost:8080/users
+app.post("/users", function(req, res) {
+  function validInput(req) {
+    return req.body.username == null || req.body.username == "" ||
+      req.body.password == null || req.body.password == "" ||
+      req.body.email == null || req.body.email == "";
+  };
+  if (validInput(req)) {
+    res.send("Ensure username, email and password were provided");
+  } else {
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.save(function(err) {
+      if (err) {
+        res.send(err.errmsg);
+      } else {
+        res.send("user created!")
+      }
+    });
+  }
+});
+
+app.listen(port, function() {
   console.log("Running the server on port:", port);
 });
